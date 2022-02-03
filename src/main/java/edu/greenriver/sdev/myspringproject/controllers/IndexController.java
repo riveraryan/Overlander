7b256defd74605151ba3150139cbf51ceedd2280
riveraryan <ryandrivera@gmail.com>
2022-02-03 12:47:11 -0800

    in IndexController.java, changed RequestMapping parameter to include value declaration.  Testing controller function in Azure Web App environment

--- src/main/java/edu/greenriver/sdev/myspringproject/controllers/IndexController.java
+++ src/main/java/edu/greenriver/sdev/myspringproject/controllers/IndexController.java
@@ -16,6 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  * @version 1.0
  */
 @Controller
+@RequestMapping
 public class IndexController
 {
     private EventService eventService;
